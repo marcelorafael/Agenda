0dc2621f7b08a50c83c95ae01a52ee445d690a06
marcelorafael <marcelorafael23@hotmail.com>
2021-05-03 19:34:07 -0300

    feat: add hash with bcryptjs

--- Agenda/src/models/RegisterModel.js
+++ Agenda/src/models/RegisterModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const RegisterSchema = new mongoose.Schema({
     email: { type: String, required: true },
@@ -17,12 +18,26 @@ class Register {
 
     async register() {
         this.valida();
-        if(this.errors.length > 0) return;
+        if (this.errors.length > 0) return;
+
+        await this.userExists();
+
+        if (this.errors.length > 0) return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
         try {
             this.user = await RegisterModel.create(this.body) //Registrando na base de dados do MongoDB
         } catch (error) {
             console.log(error);
         }
+    }
+
+    async userExists() {
+        const user = await RegisterModel.findOne({ email: this.body.email });
+
+        if(user) this.errors.push('Usuário já existe')
     }
 
     valida() {
@@ -38,7 +53,7 @@ class Register {
     }
 
     // Vamos garantir que tudo que vier dentro do body seja uma string, não queremos nada além de string
-    cleanUp(){
+    cleanUp() {
         for (const key in this.body) {
             if (typeof this.body[key] !== 'string') {
                 this.body = '';
